1142f8c793d731047e3b59e184ce91d38db91ffd
Edmar Magalhães <edmarpmc@gmail.com>
2021-01-24 14:28:14 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/trabalhocrud/TrabalhocrudApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.trabalhocrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhocrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhocrudApplication.class, args);
+	}
+
+}
