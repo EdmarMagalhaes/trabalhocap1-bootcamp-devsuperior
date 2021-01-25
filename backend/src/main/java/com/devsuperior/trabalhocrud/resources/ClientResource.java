3f6b05d2ca7abf5c441c9bb5b1a959cca3eef493
Edmar Magalhães <edmarpmc@gmail.com>
2021-01-24 22:12:11 -0300

    DTO

--- backend/src/main/java/com/devsuperior/trabalhocrud/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/trabalhocrud/resources/ClientResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.trabalhocrud.dto.ClientDTO;
 import com.devsuperior.trabalhocrud.entities.Client;
 import com.devsuperior.trabalhocrud.services.ClientSevice;
 
@@ -20,8 +21,8 @@ public class ClientResource {
 	private ClientSevice service;
 	
 	@GetMapping
-	public ResponseEntity<List<Client>> findAll() {
-		List<Client> list = service.findAll();
+	public ResponseEntity<List<ClientDTO>> findAll() {
+		List<ClientDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 		
 	}
