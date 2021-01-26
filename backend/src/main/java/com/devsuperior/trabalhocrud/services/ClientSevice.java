7dab0f2b790fbcf8783c56948a707397b7a50283
Edmar Magalhães <edmarpmc@gmail.com>
2021-01-25 21:35:28 -0300

    Delete client

--- backend/src/main/java/com/devsuperior/trabalhocrud/services/ClientSevice.java
+++ backend/src/main/java/com/devsuperior/trabalhocrud/services/ClientSevice.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.trabalhocrud.dto.ClientDTO;
 import com.devsuperior.trabalhocrud.entities.Client;
 import com.devsuperior.trabalhocrud.repositories.ClientRepository;
+import com.devsuperior.trabalhocrud.services.execeptions.DatabaseException;
 import com.devsuperior.trabalhocrud.services.execeptions.ResourceNotFoundException;
 
 @Service
@@ -56,6 +59,19 @@ public class ClientSevice {
 		catch (EntityNotFoundException e){
 			throw new ResourceNotFoundException("Id not found " + id);
 			
+		}
+		
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
 		}
 		
 	}
